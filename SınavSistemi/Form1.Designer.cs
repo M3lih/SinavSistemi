60fc7795f9f62d11c035a9d5bc985566755b0238
melih <melih@MK>
2022-04-30 22:01:31 +0300

    Proje dosyası ekle.

---
+++ SınavSistemi/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace SınavSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKullaniciSecim = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxGosterGizle = new System.Windows.Forms.CheckBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.WrongLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnOgrenciKayit = new System.Windows.Forms.Button();
+            this.btnOgrenciGiris = new System.Windows.Forms.Button();
+            this.txtOgrenciNo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 21.73585F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(699, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sınav Sistemi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.26415F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(819, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kullanıcı Türü Seçiniz";
+            // 
+            // txtKullaniciSecim
+            // 
+            this.txtKullaniciSecim.Font = new System.Drawing.Font("Segoe UI", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtKullaniciSecim.FormattingEnabled = true;
+            this.txtKullaniciSecim.Items.AddRange(new object[] {
+            "Ogrenci",
+            "Sınav Sorumlusu",
+            "Admin"});
+            this.txtKullaniciSecim.Location = new System.Drawing.Point(752, 231);
+            this.txtKullaniciSecim.Name = "txtKullaniciSecim";
+            this.txtKullaniciSecim.Size = new System.Drawing.Size(330, 33);
+            this.txtKullaniciSecim.TabIndex = 1;
+            this.txtKullaniciSecim.SelectedIndexChanged += new System.EventHandler(this.txtKullaniciSecim_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(1245, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(93, 92);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 215);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(686, 552);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBoxGosterGizle);
+            this.panel1.Controls.Add(this.btnGiris);
+            this.panel1.Controls.Add(this.txtSifre);
+            this.panel1.Controls.Add(this.txtKullaniciAdi);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.WrongLabel);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(714, 280);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 425);
+            this.panel1.TabIndex = 4;
+            // 
+            // checkBoxGosterGizle
+            // 
+            this.checkBoxGosterGizle.AutoSize = true;
+            this.checkBoxGosterGizle.Location = new System.Drawing.Point(138, 257);
+            this.checkBoxGosterGizle.Name = "checkBoxGosterGizle";
+            this.checkBoxGosterGizle.Size = new System.Drawing.Size(104, 21);
+            this.checkBoxGosterGizle.TabIndex = 5;
+            this.checkBoxGosterGizle.Text = "Sifreyi goster";
+            this.checkBoxGosterGizle.UseVisualStyleBackColor = true;
+            this.checkBoxGosterGizle.CheckedChanged += new System.EventHandler(this.checkBoxGosterGizle_CheckedChanged);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnGiris.Font = new System.Drawing.Font("Segoe UI", 12.22642F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnGiris.ForeColor = System.Drawing.Color.White;
+            this.btnGiris.Location = new System.Drawing.Point(21, 296);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(337, 42);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSifre.Font = new System.Drawing.Font("Segoe UI", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSifre.ForeColor = System.Drawing.Color.Black;
+            this.txtSifre.Location = new System.Drawing.Point(21, 208);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(337, 31);
+            this.txtSifre.TabIndex = 1;
+            this.txtSifre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSifre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Segoe UI", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtKullaniciAdi.ForeColor = System.Drawing.Color.Black;
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(21, 149);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(337, 31);
+            this.txtKullaniciAdi.TabIndex = 1;
+            this.txtKullaniciAdi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtKullaniciAdi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(125, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Kullanici Adi";
+            // 
+            // WrongLabel
+            // 
+            this.WrongLabel.AutoSize = true;
+            this.WrongLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.WrongLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.WrongLabel.Location = new System.Drawing.Point(86, 362);
+            this.WrongLabel.Name = "WrongLabel";
+            this.WrongLabel.Size = new System.Drawing.Size(197, 18);
+            this.WrongLabel.TabIndex = 0;
+            this.WrongLabel.Text = "Yanlıs Kullanıcı Adı veya Sifre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(157, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sifre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(125, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sorumlu Giris";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnOgrenciKayit);
+            this.panel2.Controls.Add(this.btnOgrenciGiris);
+            this.panel2.Controls.Add(this.txtOgrenciNo);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Location = new System.Drawing.Point(531, 252);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(384, 425);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnOgrenciKayit
+            // 
+            this.btnOgrenciKayit.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnOgrenciKayit.Font = new System.Drawing.Font("Segoe UI", 12.22642F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnOgrenciKayit.ForeColor = System.Drawing.Color.White;
+            this.btnOgrenciKayit.Location = new System.Drawing.Point(21, 293);
+            this.btnOgrenciKayit.Name = "btnOgrenciKayit";
+            this.btnOgrenciKayit.Size = new System.Drawing.Size(337, 42);
+            this.btnOgrenciKayit.TabIndex = 2;
+            this.btnOgrenciKayit.Text = "Kayıt OL";
+            this.btnOgrenciKayit.UseVisualStyleBackColor = false;
+            // 
+            // btnOgrenciGiris
+            // 
+            this.btnOgrenciGiris.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnOgrenciGiris.Font = new System.Drawing.Font("Segoe UI", 12.22642F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnOgrenciGiris.ForeColor = System.Drawing.Color.White;
+            this.btnOgrenciGiris.Location = new System.Drawing.Point(21, 233);
+            this.btnOgrenciGiris.Name = "btnOgrenciGiris";
+            this.btnOgrenciGiris.Size = new System.Drawing.Size(337, 42);
+            this.btnOgrenciGiris.TabIndex = 2;
+            this.btnOgrenciGiris.Text = "Giriş";
+            this.btnOgrenciGiris.UseVisualStyleBackColor = false;
+            // 
+            // txtOgrenciNo
+            // 
+            this.txtOgrenciNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOgrenciNo.Font = new System.Drawing.Font("Segoe UI", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtOgrenciNo.ForeColor = System.Drawing.Color.Black;
+            this.txtOgrenciNo.Location = new System.Drawing.Point(21, 149);
+            this.txtOgrenciNo.Name = "txtOgrenciNo";
+            this.txtOgrenciNo.Size = new System.Drawing.Size(337, 31);
+            this.txtOgrenciNo.TabIndex = 1;
+            this.txtOgrenciNo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtOgrenciNo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(86, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 24);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Ogrenci Giris Numara";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(86, 362);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 18);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Yanlıs Kullanıcı Adı veya Sifre";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(125, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 24);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Ogrenci Giris";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1372, 769);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtKullaniciSecim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox txtKullaniciSecim;
+        private Button btnExit;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private TextBox txtKullaniciAdi;
+        private Label label5;
+        private Label WrongLabel;
+        private Label label4;
+        private Label label3;
+        private Button btnGiris;
+        private TextBox txtSifre;
+        private CheckBox checkBoxGosterGizle;
+        private Panel panel2;
+        private Button btnOgrenciKayit;
+        private Button btnOgrenciGiris;
+        private TextBox txtOgrenciNo;
+        private Label label6;
+        private Label label7;
+        private Label label9;
+    }
+}
---
+++ SınavSistemi/Form1.cs
@@ -0,0 +1,71 @@
+namespace SınavSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            WrongLabel.Visible = false;
+        }
+
+        private void txtKullaniciSecim_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (txtKullaniciSecim.SelectedIndex == 0)
+            {
+                panel2.Visible = true;
+                panel1.Visible = false;
+            }
+            else if(txtKullaniciSecim.SelectedIndex == 1)
+            {
+                panel1.Visible = true;
+                panel2.Visible = false;   
+            }
+        }
+
+        private void checkBoxGosterGizle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxGosterGizle.Checked)
+            {
+                txtSifre.PasswordChar = '\0';
+                checkBoxGosterGizle.Text = "Şifreyi Gizle";
+
+            }
+            else
+            {
+                txtSifre.PasswordChar = '*';
+                checkBoxGosterGizle.Text = "Şifreyi Göster";
+            }
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if(txtKullaniciAdi.Text =="Sorumlu" && txtSifre.Text =="Sorumlu")
+            {
+                WrongLabel.Visible = false;
+                Sorumlu sorumlu = new Sorumlu();
+                sorumlu.Show();
+                this.Hide();
+            }
+            else
+            {
+                WrongLabel.Visible = true;
+            }
+        }
+    }
+}
